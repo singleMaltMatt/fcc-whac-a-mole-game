e343bfc04f5a663263b01358b6fc6086aa1762fb
singleMaltMatt <mattmatte93@gmail.com>
2022-03-27 16:08:00 +0200

    scripting

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const squares = document.querySelectorAll('.square');
+const mole = document.querySelector('.mole');
+const timeLeft = document.querySelector('#time-left');
+const score = document.querySelector('#score');
+
+let result = 0;
+let hitPosition;
+
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('mole')
+
+    hitPosition = randomSquare.id
+}
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++
+            score.innerHTML = result
+            hitPosition = null
+        }
+    })
+})
+
+function moveMole() {
+    let timerId = null
+    timerId = setInterval(randomSquare, 500)
+}
+
+moveMole()
