3128959530f61954a1f988969e885173e516b1f1
singleMaltMatt <mattmatte93@gmail.com>
2022-03-27 16:42:02 +0200

    completed scripting, added img for mole

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 const squares = document.querySelectorAll('.square');
 const mole = document.querySelector('.mole');
 const timeLeft = document.querySelector('#time-left');
-const score = document.querySelector('#score');
+const score = document.querySelector('.score');
 
 let result = 0;
 let hitPosition;
+let currentTime = 60;
+let timerId = null
 
 function randomSquare() {
     squares.forEach(square => {
@@ -28,8 +30,20 @@ squares.forEach(square => {
 })
 
 function moveMole() {
-    let timerId = null
     timerId = setInterval(randomSquare, 500)
 }
 
 moveMole()
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if (currentTime == 0) {
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert('Game Over! Your final score is ' + result)
+    }
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
